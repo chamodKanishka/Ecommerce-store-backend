94685846fa03e47fe53247f86652b456445f0e08
ChamodKanishka <chamodkanishka77@gmail.com>
2020-06-19 11:08:38 +0530

    backend api whole process was completed and run correctly

--- src/main/java/com/bookstore/bookstore/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.bookstore.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.bookstore.bookstore.domain.security.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
-
-}
